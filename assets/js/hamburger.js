393c4c826d2ccd290d2fea1260f2a84901467793
fadhli_fajarsyah <fadhlifajarsyah089@gmail.com>
2024-01-04 19:03:08 +0700

    day6

---
+++ assets/js/hamburger.js
@@ -0,0 +1,6 @@
+function hbr_m() {
+    const navHbr = document.getElementById("hbr");
+    
+    // Toggle class 'lk_flex' untuk .nav_hbr tanpa spasi ekstra
+    navHbr.classList.toggle('lk_flex');
+}
